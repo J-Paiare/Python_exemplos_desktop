4a446c4048a2f6cb83cdcb5c5541f6998872a748
J-Paiare <julia.p.santos15@aluno.senai.br>
2025-08-12 11:51:58 -0300

    correçao de bugs py2
    COdigo atualizado para a remoçao de bugs

--- Com interface/interface_py_2/interface_py_2.py
+++ Com interface/interface_py_2/interface_py_2.py
@@ -1,102 +1,105 @@
-#Usando bibliotca Tkinter (Padrao do Python para intefaces)
+# Usando biblioteca Tkinter (Padrão Python para interfaces)
 import tkinter as tk
 
-# Submódulo do Tikinter para widgets mais modernos e estilizados
+# Submódulo do Tkinter com widgets mais modernos e estilizados
 from tkinter import ttk
 
 def atualizar_resultado():
-    # Obter o texto da caixa de texto
+    # Obter o texto da caixa de entrada
     nome = caixa_texto.get()
-    # Obter a opção selecionada nos botões do rádio.
+
+    # Obter a opção selecionada nos botões de rádio
     preferencia = var_radio.get()
-    #Verificar se a caixa de seleção de saudação informal está marcada
+
+    # Verificar se a caixa de seleção de saudação
+    # informal esta marcada
     if var_check_saudacao.get():
-        saudacao = "Oi"
+        saudacao = "Olá"
     else:
         saudacao = "Bem-vindo"
 
+    # Verificar se a caixa de seleção de saudação
+    # personalizada esta marcada
+    if var_check_personalizada.get():
+        saudacao = f"{saudacao}, caro(a)"
 
-    #Verificar se a caixa de seleção de saudação informal está marcada
-    # persnaliza esta marcada
-    if var_check_saudacao.get():
-        saudacao = f"{saudacao}, Caro(a)"
-
-    # obter a cor favorita selecionada
+    # Obter a cor favorita selecionada
     cor_favorita = combo_cor.get() 
 
-    # Montar a mensagem final
-    mensagem += f" sua cor favorita é {cor_favorita}."
+    # Montar mensagem final
+    mensagem = f"{saudacao} {nome}!\nVocê prefere {preferencia}. "
+    if cor_favorita:
+        mensagem += f"Sua cor favorita é: {cor_favorita}. "
 
     # Atualizar o texto do rótulo de mensagem
-    Label_resultado.config(text=mensagem)
+    label_resultado.config(text=mensagem)
 
 def limpar_campos():
-    #limpa todas as caixas
+    # Limpa todas as escolhas
     caixa_texto.delete(0, tk.END)
-    var_radio.set("café")
+    var_radio.set("Café")
     var_check_saudacao.set(False)
     var_check_personalizada.set(False)
-    combo_cor.set("escolha sua cor")
-    Label_resultado.config(text="")
+    combo_cor.set("Escolha (sua cor)")
+    label_resultado.config(text="")
 
-#criar a Janela principal
-Janela = tk.Tk()
-Janela.title("Exemplo de interface")
-Janela.geometry("400x550")
-Janela.config(bg="light blue")
+# Criar a janela principal
+janela = tk.Tk()
+janela.title("Interface avançada")
+janela.geometry("400x500")
+janela.config(bg="light blue")
 
-#Criar uma caixa de entrada (entry)
-label_nome = tk.Label(Janela, text="digite seu nome")
+# Criar uma caixa de entrada (Entry)
+label_nome = tk.Label(janela, text="Digite seu nome:", bg="light blue")
 label_nome.pack(pady=5)
-caixa_texto= tk.Entry(Janela, width=60)
-caixa_texto.pack(pady=10)
+caixa_texto = tk.Entry(janela, width=40)
+caixa_texto.pack(pady=5)
 
-# criar botoes de radio
-label_preferencia = tk.Label(Janela, text="escolha sua preferencia: ")
-label_preferencia.pack(pady=5)
+# Criar botões de rádio
+label_preferencia = tk.Label(janela, text=" Escolha sua preferência:", bg="light blue")
+label_preferencia.pack(pady=10)
 
 var_radio = tk.StringVar(value="Café")
-radio_cafe = tk.Radiobutton(Janela, text="Café", variable=var_radio, value="Café")
-radio_cha = tk.Radiobutton(Janela, text="Chá", variable=var_radio, value="Chá")
-radio_suco = tk.Radiobutton(Janela, text="Suco", variable=var_radio, value="Suco")
-radio_agua = tk.Radiobutton(Janela, text="Agua", variable=var_radio, value="Agua")
+radio_cafe = tk.Radiobutton(janela, text="Café", variable=var_radio, value="Café", bg="brown", fg= "white")
+radio_cha = tk.Radiobutton(janela, text="Chá", variable=var_radio, value="Chá", bg="green", fg="black" )
+radio_suco = tk.Radiobutton(janela, text="Suco", variable=var_radio, value="Suco", bg="yellow", fg="red")
+radio_agua = tk.Radiobutton(janela, text="Água", variable=var_radio, value="Água", bg="blue", fg="white")
 
-#metohod tkinther que deixa as opçoes visiveis (.pack)
+# .pack (Método do Tkinter que
+# deixa as opções visíveis)
 radio_cafe.pack()
 radio_cha.pack()
 radio_suco.pack()
 radio_agua.pack()
 
-# criar caixas de multipla escolha
+# Criar caixas de seleção múltipla (Checkbox)
 var_check_saudacao = tk.BooleanVar()
-check_saudacao = tk.Checkbutton(Janela, text="Usar saudaçao informal", variable=var_check_saudacao)
+check_saudacao =tk.Checkbutton(janela, text="Usar saudação informal", variable=var_check_saudacao, bg="light blue", fg="magenta")
 check_saudacao.pack(pady=5)
 
 var_check_personalizada = tk.BooleanVar()
-check_personalizada = tk.Checkbutton(Janela, text="Usar saudaçao personalizada", variable=var_check_personalizada)
+check_personalizada =tk.Checkbutton(janela, text="Usar saudação personalizada", variable=var_check_personalizada, bg="light blue", fg="blue")
 check_personalizada.pack(pady=5)
 
-#comboBox (caixa com seleçao de opçoes)
-label_cor = tk.Label(Janela, text="slelecione sua cor favorita", bg="light blue")
+# ComboBox (Caixa de seleção com opções)
+label_cor = tk.Label(janela, text="Escolha sua cor favorita:")
 label_cor.pack(pady=5)
 
-combo_cor = ttk.Combobox(Janela, values= ["vermelho", "azul", "amarelo", "preto", "laranja", "verde"])
-combo_cor.set("escolha sua cor")
+combo_cor = ttk.Combobox(janela, values=["Vermelho","Azul","Verde","Amarelo","Preto","Branco"])
+combo_cor.set("Escolha (sua cor)")
 combo_cor.pack(pady=5)
 
-# botoes
-
-botao_atualizar = ttk.Button(Janela, text= "atualizar", command=atualizar_resultado)
+# Criar botões (Frase e Limpar)
+botao_atualizar = tk.Button(janela, text="Atualizar", command=atualizar_resultado)
 botao_atualizar.pack(pady=10)
 
-botao_limpar = ttk.Button(Janela, text= "limpar", command=limpar_campos)
+botao_limpar = tk.Button(janela, text="Limpar", command=limpar_campos)
 botao_limpar.pack(pady=10)
 
-#exibiçao de resultado final (rotulo "label")
-Label_resultado = tk.Label(Janela, text="", wraplength=350)
-Label_resultado.pack(pady=10)
+# Exibição do resultado final (Rótulo "Label")
+label_resultado = tk.Label(janela, text="", wraplength=300)
+label_resultado.pack(pady=10)
 
 
-#Executar a janela principal
-Janela.mainloop()
-
+# Executar a janela principal
+janela.mainloop()
